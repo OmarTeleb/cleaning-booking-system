74e7f66e864d1cbb4b3507f01b92278f1c00c658
OmarTeleb <Omartelep5@gmail.com>
2025-09-04 01:43:17 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 // استبدل هذه القيم بقيم التكوين الخاصة بك من Firebase Console
 const firebaseConfig = {
-    apiKey: "AIzaSyC9LmXn69dquTodKSoYMFGlyOIf8U1pbrg",
+  apiKey: "AIzaSyC9LmXn69dquTodKSoYMFGlyOIf8U1pbrg",
   authDomain: "cleaners-booking-system.firebaseapp.com",
   projectId: "cleaners-booking-system",
   storageBucket: "cleaners-booking-system.firebasestorage.app",
   messagingSenderId: "724475368322",
   appId: "1:724475368322:web:934663fadd9f6b97f3759b",
+  measurementId: "G-4X34M98JY7"
 };
