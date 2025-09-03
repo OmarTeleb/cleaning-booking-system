874b46473d969165adf0b1ee315d8551e9c0e627
OmarTeleb <Omartelep5@gmail.com>
2025-09-04 00:40:26 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// استبدل هذه القيم بقيم التكوين الخاصة بك من Firebase Console
+const firebaseConfig = {
+    apiKey: "AIzaSyYOUR_API_KEY_HERE",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
+};
