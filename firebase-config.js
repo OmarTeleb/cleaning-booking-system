39a6462000a9cb66e493ac7d8fd831d5e12edec2
OmarTeleb <Omartelep5@gmail.com>
2025-09-04 03:05:11 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9LmXn69dquTodKSoYMFGlyOIf8U1pbrg",
+  apiKey: "AIzaSyC9LmXn69dquTodKSoYMFGIyQIff8U1pbrg",
   authDomain: "cleaners-booking-system.firebaseapp.com",
   projectId: "cleaners-booking-system",
   storageBucket: "cleaners-booking-system.firebasestorage.app",
   messagingSenderId: "724475368322",
   appId: "1:724475368322:web:934663fadd9f6b97f3759b",
-  measurementId: "G-4X34M98JY7"
+  measurementId: "G-4X34W98JY7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
